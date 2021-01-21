a76fe3ed0e71aae1ead399235c830f6a02fece62
marcelopython <msr88957@gmail.com>
2021-01-21 00:20:22 -0300

    useContext

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
-
 import App from './views/App'
 
 ReactDOM.render(
-    <App />,
+        <App />,
     document.getElementById('root')
 )
